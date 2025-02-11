f8a3d540715808fe673c77f501649f633946ee55
zhiromustafa <kurdistan.sra@gmail.com>
2025-02-12 00:30:27 +0300

    Delete project directory

--- project/generate_qr.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Include the QR Code library (e.g., PHP QR Code)
-include('phpqrcode/qrlib.php');
-
-// Generate the QR code with a URL (the page URL where form data is displayed)
-$url = "http://zhirokrd.github.io/zhiro/submit.php"; // Change to your actual website URL
-QRcode::png($url);
-?>
